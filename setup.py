c669865ff1810ba1f7c23d281a330ba066a99059
Juha Jeronen <juha.jeronen@jyu.fi>
2017-04-25 14:56:19 +0300

    add trove classifiers for Python 3 and 3.4

--- setup.py
+++ setup.py
@@ -175,6 +175,8 @@ setup(
                     "Programming Language :: Python",
                     "Programming Language :: Python :: 2",
                     "Programming Language :: Python :: 2.7",
+                    "Programming Language :: Python :: 3",
+                    "Programming Language :: Python :: 3.4",
                     "Topic :: Scientific/Engineering",
                     "Topic :: Scientific/Engineering :: Mathematics",
                     "Topic :: Software Development :: Libraries",
